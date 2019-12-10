2c942dd7390d8b445e0009dc30082cde3ee472e9
Thesupercallum <45437634+Thesupercallum@users.noreply.github.com>
2019-12-09 20:16:19 -0800

    Averaging RSSI values to steady out. Needs testing

--- Python/GUI/i2cReceiver2.0.py
+++ Python/GUI/i2cReceiver2.0.py
@@ -13,16 +13,26 @@ address = 0x66
 count = 0
 LastRSSI = 0
 
-while True: 
+while True:
+    for i in range(10):
     
-    data = bus.read_i2c_block_data(address, 0, 5)
-    Packet1 = data[0] - 48
-    Packet2 = data[1] - 48
-    Packet3 = data[2] - 48
-    Packet4 = data[3] - 48
-    RSSI = data[4] * -1
+        data = bus.read_i2c_block_data(address, 0, 5)
+        Packet1 = data[0] - 48
+        Packet2 = data[1] - 48
+        Packet3 = data[2] - 48
+        Packet4 = data[3] - 48
+        RSSI = data[4] * -1
+        if LPacket1 == Packet1 and LPacket2 == Packet2:
+             RSSINormalize[i] = RSSI
+             time.sleep(0.1)
+        else:
+            i = 0
+        LPacket1 = Packet1
+        LPacket2 = Packet2
+       
+    NormRSSI = sum(RSSINormalized)/10
     
-    if RSSI is not LastRSSI:
+    if NormRSSI is not LastRSSI:
         userID = str(Packet1)+str(Packet2)
         print('Packet is: ', Packet1, Packet2, Packet3, Packet4)
         print('RSSI is: {} dBm'.format(RSSI))
@@ -36,7 +46,7 @@ while True:
         count+=1
         print('Count: ',count)
         print('*****************************************************************')
-        LastRSSI = RSSI
+        LastRSSI = NormRSSI
         
     else:
         userID = str(Packet1)+str(Packet2)
@@ -53,8 +63,6 @@ while True:
         print('Count: ',count)
         print('*****************************************************************')
 
-#     print('Packet: ', data1, data2, data3, data4)
-#     print('rssi = ', rssi)
-    time.sleep(1)
+
     
     
